36cde59bfa50db287031eb02e8c50569051bbe92
edi_bice <edi_bice@yahoo.com>
2016-02-18 17:06:12 -0500

    A tentative solution to issue described in:
    https://issues.apache.org/jira/browse/SAMOA-58

--- samoa-api/src/main/java/org/apache/samoa/streams/FileStream.java
+++ samoa-api/src/main/java/org/apache/samoa/streams/FileStream.java
@@ -81,7 +81,16 @@ public abstract class FileStream extends AbstractOptionHandler implements Instan
 
   @Override
   public boolean hasMoreInstances() {
-    return !this.hitEndOfStream;
+    if (this.hitEndOfStream) {
+      if (getNextFileReader()) {
+        this.hitEndOfStream = false;
+        return hasMoreInstances();
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
   }
 
   @Override
